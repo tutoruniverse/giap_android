fb62e4ab0bd2ee475e078208efc490bd8dbdfabb
Tran Viet Thang <uendno@gmail.com>
2020-02-17 23:17:23 +0700

    feat: Add demo

--- example/src/main/java/ai/gotit/giap_android/MainActivity.java
+++ example/src/main/java/ai/gotit/giap_android/MainActivity.java
@@ -2,41 +2,39 @@ package ai.gotit.giap_android;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.os.StrictMode;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import ai.gotit.giap.GIAP;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-    private Integer count = 0;
+    private Button loginButton;
+    private Button signUpButton;
+    private Button visitButton;
+    private Button askButton;
+    private Button setFullNameButton;
+    private Button logoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        loginButton = findViewById(R.id.login_button);
+        signUpButton = findViewById(R.id.sign_up_button);
+        visitButton = findViewById(R.id.visit_button);
+        askButton = findViewById(R.id.ask_button);
+        setFullNameButton = findViewById(R.id.set_full_name_button);
+        logoutButton = findViewById(R.id.logout_button);
 
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
+        setAuthState(false);
 
         final GIAP giap = GIAP.initialize(
                 "https://5e4a53406eafb7001488c319.mockapi.io",
@@ -44,48 +42,145 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this
         );
 
-        Button btn = findViewById(R.id.click_me_btn);
-        btn.setOnClickListener(new View.OnClickListener() {
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                //showAlert("Clicked");
-                giap.track("Hello event");
+            public void onClick(View v) {
+                showTextInputDialog(InputType.TYPE_CLASS_TEXT, "User ID", new OnSubmitDialogListener() {
+                    @Override
+                    public void onSubmit(String text) {
+                        giap.identify(text);
+                        setAuthState(true);
+                    }
+                });
+            }
+        });
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTextInputDialog(InputType.TYPE_CLASS_TEXT, "User ID", new OnSubmitDialogListener() {
+                    @Override
+                    public void onSubmit(String text) {
+                        giap.alias(text);
+                        setAuthState(true);
+                    }
+                });
+            }
+        });
+
+        visitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTextInputDialog(InputType.TYPE_CLASS_NUMBER, "Economy group", new OnSubmitDialogListener() {
+                    @Override
+                    public void onSubmit(String text) {
+                        try {
+                            JSONObject props = new JSONObject();
+                            props.put("economy_group", Integer.parseInt(text, 10));
+                            giap.track("Visit", props);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        });
+
+        askButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTextInputDialog(InputType.TYPE_CLASS_NUMBER, "Problem text", new OnSubmitDialogListener() {
+                    @Override
+                    public void onSubmit(String text) {
+                        try {
+                            JSONObject props = new JSONObject();
+                            props.put("problem_text", text);
+                            giap.track("Ask", props);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        });
+
+        setFullNameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTextInputDialog(InputType.TYPE_CLASS_NUMBER, "Full name", new OnSubmitDialogListener() {
+                    @Override
+                    public void onSubmit(String text) {
+                        try {
+                            JSONObject props = new JSONObject();
+                            props.put("full_name", text);
+                            //giap.setProfileProperty(props);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        });
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // giap.reset();
+                setAuthState(false);
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private void setAuthState(boolean loggedIn) {
+        if (loggedIn) {
+            loginButton.setVisibility(Button.GONE);
+            signUpButton.setVisibility(Button.GONE);
+            visitButton.setVisibility(Button.GONE);
+            askButton.setVisibility(Button.VISIBLE);
+            setFullNameButton.setVisibility(Button.VISIBLE);
+            logoutButton.setVisibility(Button.VISIBLE);
+        } else {
+            loginButton.setVisibility(Button.VISIBLE);
+            signUpButton.setVisibility(Button.VISIBLE);
+            visitButton.setVisibility(Button.VISIBLE);
+            askButton.setVisibility(Button.GONE);
+            setFullNameButton.setVisibility(Button.GONE);
+            logoutButton.setVisibility(Button.GONE);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
-    public void showAlert(String text) {
-        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
-        alertDialog.setTitle("Alert");
-        alertDialog.setMessage(text);
-        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                });
-        alertDialog.show();
+    private void showTextInputDialog(int inputType, String title, final OnSubmitDialogListener onSubmit) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title);
+
+        // Set up the input
+        final EditText input = new EditText(this);
+        // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        input.setInputType(inputType);
+
+        builder.setView(input);
+
+        // Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                onSubmit.onSubmit(input.getText().toString());
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+    interface OnSubmitDialogListener {
+        void onSubmit(String text);
     }
 }
