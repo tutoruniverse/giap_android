dfae4160629f28560063580e2650fc0d6208cceb
nam-hackintosh <lenkc98@gmail.com>
2020-02-25 23:23:12 +0700

    test: add tests for ConfigManager

---
+++ giap/src/test/java/ai/gotit/giap/ConfigManagerTest.java
@@ -0,0 +1,68 @@
+package ai.gotit.giap;
+
+import android.app.Activity;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ai.gotit.giap.service.ConfigManager;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class ConfigManagerTest {
+    private ConfigManager configManager;
+    private final Activity context = mock(Activity.class);
+    private final String serverUrl = "test_server_url";
+    private final String token = "test_token";
+
+    @Before
+    public void beforeEach() throws Exception {
+        configManager = spy(ConfigManager.makeInstance(context, serverUrl, token));
+    }
+
+    @Test
+    public void makeInstance() {
+        ConfigManager newInstance = ConfigManager.makeInstance(context, serverUrl, token);
+        assertNotSame(newInstance, configManager);
+        assertEquals(newInstance.getServerUrl(), serverUrl);
+        assertEquals(newInstance.getToken(), token);
+        assertSame(newInstance.getContext(), context);
+    }
+
+    @Test
+    public void getContext() {
+        assertSame(configManager.getContext(), context);
+    }
+
+    @Test
+    public void setContext() {
+        Activity newContext = mock(Activity.class);
+        configManager.setContext(newContext);
+        assertSame(configManager.getContext(), newContext);
+    }
+
+    @Test
+    public void getServerUrl() {
+        assertEquals(configManager.getServerUrl(), serverUrl);
+    }
+
+    @Test
+    public void setServerUrl() {
+        String newServerUrl = "new_server_url";
+        configManager.setServerUrl(newServerUrl);
+        assertEquals(configManager.getServerUrl(), newServerUrl);
+    }
+
+    @Test
+    public void getToken() {
+        assertEquals(configManager.getToken(), token);
+    }
+
+    @Test
+    public void setToken() {
+        String newToken = "new_token";
+        configManager.setToken(newToken);
+        assertEquals(configManager.getToken(), newToken);
+    }
+}
