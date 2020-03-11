6f8b1b674efa0397622e94f4def9923897b6e2f9
nam-hackintosh <lenkc98@gmail.com>
2020-03-11 11:13:24 +0700

    feat: add sdkVersion to Task, then ignore if stored task is from old version

--- giap/src/main/java/ai/gotit/giap/entity/Task.java
+++ giap/src/main/java/ai/gotit/giap/entity/Task.java
@@ -3,15 +3,18 @@ package ai.gotit.giap.entity;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import ai.gotit.giap.BuildConfig;
 import ai.gotit.giap.common.Serializable;
 import ai.gotit.giap.constant.TaskProps;
 
 public class Task implements Serializable {
     private String type;
     private JSONObject data = null;
+    private String sdkVersion = null;
     private Boolean processing = false;
 
     public Task(String type) {
+        this.sdkVersion = BuildConfig.VERSION_NAME;
         this.type = type;
     }
 
@@ -22,7 +25,16 @@ public class Task implements Serializable {
 
     public Task(JSONObject serializedTask) throws JSONException {
         type = serializedTask.getString(TaskProps.TASK_TYPE);
-        data = serializedTask.getJSONObject(TaskProps.DATA);
+        try {
+            data = serializedTask.getJSONObject(TaskProps.DATA);
+        } catch (JSONException e) {
+            data = null;
+        }
+        try {
+            sdkVersion = serializedTask.getString(TaskProps.SDK_VERSION);
+        } catch (JSONException e) {
+            sdkVersion = null;
+        }
     }
 
     public String getType() {
@@ -37,6 +49,10 @@ public class Task implements Serializable {
         this.data = data;
     }
 
+    public String getSdkVersion() {
+        return sdkVersion;
+    }
+
     public Boolean getProcessing() {
         return processing;
     }
@@ -49,6 +65,7 @@ public class Task implements Serializable {
         JSONObject json = new JSONObject();
         json.put(TaskProps.TASK_TYPE, type);
         json.put(TaskProps.DATA, data);
+        json.put(TaskProps.SDK_VERSION, sdkVersion);
         return json;
     }
 }
